3ac0f4be93d8298ed7a2c15b7bfbf51d3c1db700
1one2two <ej04vm6@gmail.com>
2019-07-16 23:21:03 +0800

    Fix

--- setup.sh
+++ setup.sh
@@ -3,24 +3,21 @@
 #       Install HLJH
 # History:
 # 2019/06/27	
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+#PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+#export PATH
 
-echo "Start install \a \n"
+#echo "Start install \a \n"
 sudo i2cdetect -y 1
 
 cd ~
 git clone https://github.com/Seeed-Studio/grove.py
 
-cd grove.py/
-sudo pip install .
+cd grove.py/ && sudo pip install .
 
 sudo pip install hcsr04sensor
 
 curl https://raw.githubusercontent.com/1one2two/hljh/master/hljh.py -o /grove.py/grove/
 
 #Download node red
-
-#Set hostname
 
 exit 0
